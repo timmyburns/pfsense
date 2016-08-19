c672acce3adfaa933ec818ed40f8a319266fb5ec
timmy burns <timothyfwebster@gmail.com>
2016-08-18 21:40:13 -0400

    first malware source link

---
+++ mal_domains.py
@@ -0,0 +1,17 @@
+'''
+
+
+Script to download from several sources and normalize the domain data within.
+
+Script should be run daily for maximum freshness.
+
+This script is intended to be run on a pfSense box for auto-miscreant-punchery.
+
+Authors: timmyburns, t0n1k
+Email: timothyfwebster@gmail.com
+Version: 0.1a
+
+
+'''
+
+http://www.malwaredomainlist.com/mdlcsv.php
